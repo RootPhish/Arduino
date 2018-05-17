f63ca4ff8bf3464d41b8ea4da4e18549e529a6cf
Wouter Sonneveldt <wouter@Wouters-MacBook.fritz.box>
2018-05-17 23:03:47 +0200

    Fixed scrolling

--- Ether-Ticker/Ether-Ticker.ino
+++ Ether-Ticker/Ether-Ticker.ino
@@ -6,6 +6,8 @@
 
 const char* host = "ws-feed.gdax.com";
 const int httpsPort = 443;
+const int displayWidth = 128;
+const int displayHeight = 32;
 
 U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C u8g2(U8G2_R0);
 WebSocketsClient webSocket;
@@ -16,7 +18,7 @@ unsigned long startMillis;
 unsigned long currentMillis;
 const unsigned long period = 5000;
 
-int activePage = 1;
+int activePage = 0;
 int numPages = 2;
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
@@ -103,9 +105,9 @@ void drawStr2Lines(const char *s1, const char *s2) {
   do {
     u8g2.setFont(u8g2_font_crox3h_tf);
     width = u8g2.getStrWidth(s1);
-    u8g2.drawStr((128 - width) / 2, 12, s1);
+    u8g2.drawStr((displayWidth - width) / 2, 12, s1);
     width = u8g2.getStrWidth(s2);
-    u8g2.drawStr((128 - width) / 2, 31, s2);
+    u8g2.drawStr((displayWidth - width) / 2, 31, s2);
   } while ( u8g2.nextPage() );
 }
 
@@ -115,40 +117,67 @@ void drawStr1Line(const char *s, int ypos = 31) {
   do {
     u8g2.setFont(u8g2_font_inr21_mf);
     width = u8g2.getStrWidth(s);
-    u8g2.drawStr((128 - width) / 2, ypos, s);
+    u8g2.drawStr((displayWidth - width) / 2, ypos, s);
   } while ( u8g2.nextPage() );  
 }
 
-void drawAndScroll(const char *s) {
-  int ypos = 31;
-  while (ypos > -5) {
-    drawStr1Line(s, ypos);
+void drawAndScroll(const char *s, const char *next) {
+  int gap = 5;
+  int ypos = displayHeight - 1;
+  int width;
+  while (ypos > -gap) {
+    u8g2.firstPage();
+    do {
+      u8g2.setFont(u8g2_font_inr21_mf);
+      width = u8g2.getStrWidth(s);
+      u8g2.drawStr((displayWidth - width) / 2, ypos, s);
+      width = u8g2.getStrWidth(next);
+      u8g2.drawStr((displayWidth - width) / 2, ypos + displayHeight - 1 + gap, next);
+    } while ( u8g2.nextPage() ); 
     ypos--;
     delay(10);
   }
 }
-void printData() {
-  char buffer[16];
-  int ret;
 
-  switch (activePage) {
-    case 1: ret = snprintf(buffer, sizeof buffer, "$%.2f", currentValue);
+char *getContentForPage(int page) {
+  char *buffer;
+  int ret;
+  size_t needed;
+  
+  switch (page) {
+    case 0: needed = snprintf(NULL, 0, "$%.2f", currentValue) + 1;
+            buffer = (char *)malloc(needed);
+            ret = snprintf(buffer, needed, "$%.2f", currentValue);
             break;
-    case 2: ret = snprintf(buffer, sizeof buffer, "%+.2f%%", currentPL);
+    case 1: needed = snprintf(NULL, 0, "%+.2f%%", currentPL) + 1;
+            buffer = (char *)malloc(needed);
+            ret = snprintf(buffer, needed, "%+.2f%%", currentPL);
             break;
   }
+  return buffer;
+}
 
+void printData() {
+  char *buffer = getContentForPage(activePage);
+  Serial.println("current:");
+  Serial.println(buffer);
   currentMillis = millis();
   if (currentMillis - startMillis >= period) {
-    drawAndScroll(buffer);
+    char *nextBuffer = getContentForPage((activePage + 1) % numPages);
+    Serial.println("next:");
+    Serial.println(nextBuffer);
+    drawAndScroll(buffer, nextBuffer);
     activePage++;
-    if (activePage > numPages) {
-      activePage = 1;
+    if (activePage >= numPages) {
+      activePage = 0;
     }
     startMillis = currentMillis;
+    free(nextBuffer);
   } else {
     drawStr1Line(buffer);
   }
+
+  free(buffer);
 }
 
 void loop() {
